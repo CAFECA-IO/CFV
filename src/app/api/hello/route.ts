7d4c7f29f679f80c33d5ae312b0e0a7046ce2a92
Luphia Chang <luphia@cafeca.io>
2023-04-06 00:24:30 +0800

    workaround version

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,24 +1,89 @@
 import XLSX, { read } from 'xlsx';
 
+// sleep function
+const sleep = async (ms: number) => {
+  console.log('sleep: ', ms);
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const removeSymbol = (address: string) => {
+  return address.replaceAll('#', '');
+}
+
+const getDistance = async (address1: string, address2: string, retry = 3) => {
+  try {
+    // get distance from google api with address1 and address2
+    const cleanAddress1 = removeSymbol(address1);
+    const cleanAddress2 = removeSymbol(address2);
+    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${cleanAddress1}&destinations=${cleanAddress2}&key=AIzaSyC3Sn90tnB460mmoRUsAk04rhTeafprVBY`;
+    console.log(url)
+    const response = await fetch(url);
+    const data = await response.json();
+    const address1Parsed = data.origin_addresses[0];
+    const address2Parsed = data.destination_addresses[0];
+    const distance = data.rows[0].elements[0].distance.text;
+    const result = {
+      address1: address1Parsed,
+      address2: address2Parsed,
+      distance: distance
+    };
+    return result;
+  } catch (error) {
+    console.log(error);
+    await sleep(1000);
+    const retryTime = retry - 1;
+    if (retryTime === 0) {
+      return {
+        address1: 'Unknown Address',
+        address2: 'Unknown Address',
+        distance: '?? km'
+      };
+    } else {
+      return getDistance(address1, address2, retryTime);
+    }
+  }
+}
+
+const parseSheet = async (sheet: any) => {
+  const json = XLSX.utils.sheet_to_json(sheet);
+  const result: any[] = [];
+
+  let count = 0;
+  for(let row of json) {    
+    const values = Object.values(row || {});
+    const newRecord: any = row;
+    const address1 = values[1] as string;
+    const address2 = values[2] as string;
+    const distance = await getDistance(address1, address2);
+    
+    newRecord['Origin'] = distance.address1;
+    newRecord['Destination'] = distance.address2;
+    newRecord['Distance'] = distance.distance;
+    result.push(newRecord);
+
+    const sleepTime = Math.ceil(Math.random() * 100 + 100);
+    await sleep(sleepTime);
+  }
+
+  const rsSheet = XLSX.utils.json_to_sheet(result);
+  return rsSheet;
+}
+
 export async function GET(request: Request) {
   // read address from xlsx file
-  const workbook = await read('src/xlsx/air.xlsx', { type: 'file' });
-  const sheet = workbook.Sheets[workbook.SheetNames[0]];
-  const json = XLSX.utils.sheet_to_json(sheet);
+  const workbook = await read('src/xlsx/ground.xlsx', { type: 'file' });
+  const newWorkbook = XLSX.utils.book_new();
+  const sheetList = workbook.SheetNames;
+  
+  for(let sheetName of sheetList) {
+    const sheet = workbook.Sheets[sheetName];
+    const rsSheet = await parseSheet(sheet);
+    XLSX.utils.book_append_sheet(newWorkbook, rsSheet, sheetName);
+    const sleepTime = Math.ceil(Math.random() * 500);
+    await sleep(sleepTime);
+  }
+  XLSX.writeFile(newWorkbook, 'src/xlsx/ground_result.xlsx');
 
-  const record1 = Object.values(json[1]);
-  const address1 = record1[3];
-  const address2 = record1[7];
-
-  // get distance from google api with address1 and address2
-  const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${address1}&destinations=${address2}&key=AIzaSyC3Sn90tnB460mmoRUsAk04rhTeafprVBY`;
-  const response = await fetch(url);
-  const data = await response.json();
-
-  // get distance from google distance api 
-  const url2 = `https://maps.googleapis.com/maps/api/distance/json?units=metric&origins=${address1}&destinations=${address2}&key=AIzaSyC3Sn90tnB460mmoRUsAk04rhTeafprVBY`;
-  const response2 = await fetch(url2);
-  const data2 = await response2.json();
-
-  return new Response(JSON.stringify(data2));
+  const response = new Response('DONE!');
+  return response;
 }
