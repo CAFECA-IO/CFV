3ef2a1a56e68e69d67acc11dcb7b8954a9eb752d
Julian <julian890701@gmail.com>
2023-10-24 14:07:34 +0800

    develop menuview

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,29 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 import Image from 'next/image';
-import {Inter} from 'next/font/google';
-import styles from '../styles/main.module.css';
 import {SessionProvider} from 'next-auth/react';
 import type {AppProps} from 'next/app';
 import {GoogleOAuthProvider} from '@react-oauth/google';
-
 import {useSession, signIn, signOut} from 'next-auth/react';
-import React from 'react';
 import {GoogleLogin} from '@react-oauth/google';
+import {PiHouseBold} from 'react-icons/pi';
+import {LuUsers} from 'react-icons/lu';
 
 const Main = () => {
   const {data: session, status} = useSession();
   const content = !session ? <OperationView session={session} /> : <LoginView />;
-  const view = <div className={styles.container}>{content}</div>;
+  const view = (
+    <div className="p-10px font-roboto flex min-h-screen max-w-1032px mx-auto justify-center items-center">
+      {content}
+    </div>
+  );
   return view;
 };
 
 const LoginView = () => {
   const view = (
-    <div className={styles.loginView}>
-      <div className={styles.loginBlock}>
+    <div className="flex flex-col justify-center items-center h-720px w-full bg-white rounded">
+      <div className="ml-530px w-560px h-250px 80px text-2xl font-bold">
         <GoogleLogin
           onSuccess={credentialResponse => {
             console.log(credentialResponse);
@@ -39,30 +42,58 @@ const LoginView = () => {
 const OperationView = session => {
   const session_data = JSON.stringify(session);
   const view = (
-    <div className={styles.operationView}>
-      <MenuView></MenuView>
-      <JobBoard></JobBoard>
+    <div className="flex items-start justify-start w-full h-720px rounded bg-white">
+      <MenuView />
+      <JobBoard />
     </div>
   );
 
   return view;
 };
+
 const MenuView = () => {
   const view = (
-    <div className={styles.menuView}>
-      <div className={styles.menuLogo}></div>
-      <div className={styles.menuBlock}>
-        <div className={styles.menuItem}>1</div>
-        <div className={styles.menuItem}>2</div>
-        <div className={styles.menuItem}>3</div>
-        <div className={styles.menuItem}>4</div>
+    <div className="w-250px flex flex-col space-y-20 items-center h-full rounded px-4 py-6 bg-white shadow-xl">
+      {/* Info: (20231024 - Julian) Logo */}
+      <div>
+        <Image src="/tsmc_logo.svg" width={100} height={80} alt="tsmc_logo" />
+      </div>
+      {/* Info: (20231024 - Julian) User Info */}
+      <div className="flex space-x-4 items-center">
+        {/* Info: (20231024 - Julian) Avatar */}
+        <div className="rounded-full overflow-hidden relative w-50px h-50px">
+          <Image
+            src={'/avatar/user_photo_1.jpeg'}
+            fill
+            style={{objectFit: 'cover', objectPosition: 'top'}}
+            alt="avatar"
+          />
+        </div>
+        {/* Info: (20231024 - Julian) User Name & Email */}
+        <div className="flex flex-col">
+          <h2 className="text-sm text-black">Jane Doe</h2>
+          <p className="text-gray text-xs">jane.doe@gmail.com</p>
+        </div>
+      </div>
+      {/* Info: (20231024 - Julian) Menu Items */}
+      <div className="flex flex-col w-full">
+        {/* Info: (20231024 - Julian) Overview */}
+        <button className="flex w-full text-white font-bold border border-primaryGreen text-base items-center space-x-2 p-3 bg-primaryGreen">
+          <PiHouseBold size={24} />
+          <p>Overview</p>
+        </button>
+        {/* Info: (20231024 - Julian) Collaborators */}
+        <button className="flex w-full text-black2 border font-bold border-gray2 text-base items-center space-x-2 p-3 bg-white">
+          <LuUsers size={24} />
+          <p>Collaborators</p>
+        </button>
       </div>
     </div>
   );
   return view;
 };
 const JobBoard = () => {
-  const view = <div className={styles.jobBoard}></div>;
+  const view = <div className="flex h-full bg-white2"></div>;
   return view;
 };
 
@@ -71,11 +102,18 @@ const App = ({Component, pageProps: {session, ...pageProps}}: AppProps) => {
   const googleSecret = process.env.GOOGLE_SECRET || '';
 
   return (
-    <SessionProvider session={session}>
-      <GoogleOAuthProvider clientId={googleId}>
-        <Main />
-      </GoogleOAuthProvider>
-    </SessionProvider>
+    <>
+      <Head>
+        <title>TSMS</title>
+        <link rel="icon" href="/tsms.ico" />
+      </Head>
+
+      <SessionProvider session={session}>
+        <GoogleOAuthProvider clientId={googleId}>
+          <Main />
+        </GoogleOAuthProvider>
+      </SessionProvider>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,40 @@ module.exports = {
     extend: {
       colors: {
         coolGray: '#DDE1E6',
+        gray: '#697077',
+        gray2: '#F2F4F8',
+
+        white: '#FFFFFF',
+        white2: '#fcfcfc',
+
+        black: '#101010',
+        black2: '#21272A',
 
         primaryGreen: '#57BE6C',
+      },
+      spacing: {
+        '1/2': '50%',
+        '1/3': '33.333333%',
+        '2/3': '66.666667%',
+
+        '10px': '10px',
+        '50px': '50px',
+        '80px': '80px',
+        '150px': '150px',
+        '250px': '250px',
+        '530px': '530px',
+        '560px': '560px',
+        '720px': '720px',
+        '1032px': '1032px',
+      },
+      maxWidth: {
+        '1032px': '1032px',
+      },
+      boxShadow: {
+        xl: '4px 0px 24px 0px rgba(0, 39, 0, 0.10)',
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
       },
     },
   },
