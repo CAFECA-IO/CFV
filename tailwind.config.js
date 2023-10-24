beaa5ee33e78d8d2894a4a8fee5235597956b771
Julian <julian890701@gmail.com>
2023-10-24 11:08:50 +0800

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      colors: {
+        coolGray: '#DDE1E6',
+
+        primaryGreen: '#57BE6C',
+      },
+    },
+  },
+  plugins: [],
+};
